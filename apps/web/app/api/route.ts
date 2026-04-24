d19da07a26220d226ce92080c278275fc05afe98
modichika <shristmodi95@gmail.com>
2026-04-24 18:30:14 +0000

    feat: add POST handler and zod validation to api

--- apps/web/app/api/route.ts
+++ apps/web/app/api/route.ts
@@ -1,5 +1,20 @@
 import { connectDb, tasks } from "@repo/db";
+import { TaskInsertSchema } from "../../../../packages/db/src/schema";
 // Remove: import { env } from "process";
+
+export async function POST(request: Request) {
+  const db = await connectDb(process.env as any);
+  const body: { name: string; description: string }  = await request.json();
+  const validatedData = TaskInsertSchema.parse(body);
+  // Insert the task into Neon
+  const result = await db.insert(tasks).values({
+    name: validatedData.name,
+    description: validatedData.description,
+  }).returning();
+
+  return Response.json(result);
+}
+
 
 export async function GET() {
   try {
