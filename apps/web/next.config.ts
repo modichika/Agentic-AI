ead4f04828e12d27f564a41dc35160a4d6f742a3
modichika <shristmodi95@gmail.com>
2026-04-25 14:51:17 +0000

    only run initOpenNextCloudflareForDev on local

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,9 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 // This must run BEFORE the config object is defined
-initOpenNextCloudflareForDev();
+if (process.env.NODE_ENV === 'development') {
+  initOpenNextCloudflareForDev();
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
